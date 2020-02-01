7383503482cd21a86d3a0f13ea1c8cdd3ee4515c
gustavo <gustavorw03@gmail.com>
2020-02-01 14:32:50 -0300

    iniciado a api-rest criada a função de adicionar usuarios

---
+++ reserva-api/env/lib/python3.6/site-packages/marshmallow_sqlalchemy/__init__.py
@@ -0,0 +1,24 @@
+from .schema import TableSchemaOpts, ModelSchemaOpts, TableSchema, ModelSchema
+
+from .convert import (
+    ModelConverter,
+    fields_for_model,
+    property2field,
+    column2field,
+    field_for,
+)
+from .exceptions import ModelConversionError
+
+__version__ = "0.21.0"
+__all__ = [
+    "TableSchema",
+    "ModelSchema",
+    "TableSchemaOpts",
+    "ModelSchemaOpts",
+    "ModelConverter",
+    "fields_for_model",
+    "property2field",
+    "column2field",
+    "ModelConversionError",
+    "field_for",
+]
---
+++ reserva-api/env/lib/python3.6/site-packages/marshmallow_sqlalchemy/exceptions.py
@@ -0,0 +1,10 @@
+class MarshmallowSQLAlchemyError(Exception):
+    """Base exception class from which all exceptions related to
+    marshmallow-sqlalchemy inherit.
+    """
+
+
+class ModelConversionError(MarshmallowSQLAlchemyError):
+    """Raised when an error occurs in converting a SQLAlchemy construct
+    to a marshmallow object.
+    """
