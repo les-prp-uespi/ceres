06122af792a3da4653b405c656a13df8e2081f40
gustavo-wanderley <gustavorw03@gmail.com>
2021-03-08 08:50:29 -0300

    deletado appdrawer

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+//import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:uespi_reserva/provider/auth_provider.dart';
-import 'package:uespi_reserva/provider/controller_provider.dart';
-import 'package:uespi_reserva/util/name_routes.dart';
-import 'package:uespi_reserva/views/auth_home_view.dart';
-import 'package:uespi_reserva/views/auth_view.dart';
-import 'package:uespi_reserva/views/create_reservation_view.dart';
-import 'package:uespi_reserva/views/home_view.dart';
-import 'package:uespi_reserva/views/reservations_view.dart';
-import 'package:uespi_reserva/views/resource_view.dart';
+import 'package:uespi_reserva/app_modular.dart';
 
-import 'views/update_reservation_view.dart';
 
 void main() {
-  initializeDateFormatting().then((_) => runApp(MyApp()));
+  initializeDateFormatting().then(
+    (_) => runApp(
+      ModularApp(
+        module: AppModule(),
+      ),
+    ),
+  );
 }
-
+/*
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -47,3 +45,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
